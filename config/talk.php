90f8cd7ddaea8e89a52359e977d02a53fc409e82
gaspan <gaspan619@gmail.com>
2017-02-25 22:37:00 +0700

    update

--- config/talk.php
+++ config/talk.php
@@ -4,12 +4,12 @@ return [
         'model' => 'App\User'
     ],
     'broadcast' => [
-        'enable' => false,
+        'enable' => true,
         'app_name' => 'talk-example',
         'pusher' => [
-            'app_id'        => env('PUSHER_APP_ID'),
-            'app_key'       => env('PUSHER_KEY'),
-            'app_secret'    => env('PUSHER_SECRET')
+            'app_id'        => env('PUSHER_APP_ID','1a'),
+            'app_key'       => env('PUSHER_KEY','1a'),
+            'app_secret'    => env('PUSHER_SECRET','1a')
         ]
     ]
 ];
